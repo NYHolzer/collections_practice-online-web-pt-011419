2b35a347950e7a46d52a4c5bdc8cdd63b746ee31
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-27 20:50:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,5 +45,5 @@ def sum_array(array)
 end 
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| if element [index<>1] << "s"}
+  array.each_with_index.collect {|element, index| if element [index<> 1] << "s"}
 end 
